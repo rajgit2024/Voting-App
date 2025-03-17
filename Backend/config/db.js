b15ed27ec5f429712424f30d0e77cea9d71b400d
raj dubey <rajdu590@gmail.com>
2025-03-17 14:49:07 +0530

    Using url in config

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,14 +2,16 @@ const {Pool}=require("pg");
 require('dotenv').config(); 
 
 const pool=new Pool({
-   user:process.env.PG_USER,
-   database:process.env.PG_DATABASE,
-   password:process.env.PG_PASSWORD,
-   port:process.env.PG_PORT,
-   host:process.env.PG_HOST,
-   ssl: {
-      rejectUnauthorized: false, // Important for Supabase and Render
+   // user:process.env.PG_USER,
+   // database:process.env.PG_DATABASE,
+   // password:process.env.PG_PASSWORD,
+   // port:process.env.PG_PORT,
+   // host:process.env.PG_HOST,
+   connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false, // Required for Railway
     },
+   //   connectionString: `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}?hostaddr=0.0.0.0`
 })
 
 module.exports=pool;
