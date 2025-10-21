235189a85664aa885abb394caa9ae89447336328
raj dubey <rajdu590@gmail.com>
2025-10-21 11:19:06 +0530

    Frontend env updation

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
-  console.log("API BASE URL:",BASE_URL)
+
+console.log("API BASE URL:", process.env.REACT_APP_BASE_URL);
 
 export default API;
